c57e38d8b3247011d18b31e959df3070c0c565e1
Tibo Ulens <ulens.tibo@gmail.com>
2022-03-01 03:07:38 +0100

    make rasterize a method of VertexBuffer

--- src/raster.rs
+++ src/raster.rs
@@ -2,29 +2,31 @@
 
 use crate::{Config, Pixel, PixelBuffer, Primitive, VertexBuffer};
 
-/// Rasterize the given list of vertices using Bresenham's algorithm
-///
-/// ## Arguments
-/// - cfg: [Config](super::Config) - The renderer configuration
-/// - buff: [VertexBuffer](super::VertexBuffer) - The list of vertices to draw
-pub fn rasterize_scan(cfg: &Config, buff: &VertexBuffer) -> PixelBuffer {
-	let mut pixels = Vec::<Pixel>::new();
+impl VertexBuffer {
+	/// Rasterize the given list of vertices using Bresenham's algorithm
+	///
+	/// ## Arguments
+	/// - cfg: [Config](super::Config) - The renderer configuration
+	/// - buff: [VertexBuffer](super::VertexBuffer) - The list of vertices to draw
+	pub fn rasterize_scan(&self, cfg: &Config) -> PixelBuffer {
+		let mut pixels = Vec::<Pixel>::new();
 
-	match cfg.primitive {
-		Primitive::Point => {
-			pixels = buff
-				.iter()
-				.map(|v| {
-					Pixel {
-						x:     v.0,
-						y:     v.1,
-						color: (255, 255, 255, 255),
-					}
-				})
-				.collect();
-		},
-		_ => (),
+		match cfg.primitive {
+			Primitive::Point => {
+				pixels = self
+					.iter()
+					.map(|v| {
+						Pixel {
+							x:     v.0,
+							y:     v.1,
+							color: (255, 255, 255, 255),
+						}
+					})
+					.collect();
+			},
+			_ => (),
+		}
+
+		pixels.into()
 	}
-
-	pixels.into()
 }
