ab0ea9d2c358ad42ef9ae187c0ffeb4203c499e2
Tibo Ulens <ulens.tibo@gmail.com>
2022-03-01 20:03:16 +0100

    draw fancy things

--- src/main.rs
+++ src/main.rs
@@ -5,15 +5,41 @@ use kelder_scherm_renderer::{Config, Primitive, Vertex, VertexBuffer};
 async fn main() -> Result<(), RenderError> {
 	let cfg = Config::new(600, 800, Primitive::TriangleStrip);
 
-	let verts = vec![Vertex(240, 290), Vertex(250, 290), Vertex(245, 280), Vertex(255, 280)];
+	let left_side = vec![
+		Vertex(44, 59),
+		Vertex(56, 59),
+		Vertex(50, 50),
+		Vertex(61, 50),
+		Vertex(56, 44),
+	];
 
-	let mut vbuf = VertexBuffer::from(verts);
+	let right_side = vec![
+		Vertex(44, 59),
+		Vertex(50, 50),
+		Vertex(39, 50),
+		Vertex(44, 44),
+		Vertex(56, 44),
+	];
 
-	let pbuf = vbuf.rasterize_scan(&cfg);
+	let top = vec![
+		Vertex(44, 44),
+		Vertex(56, 44),
+		Vertex(50, 53),
+	];
+
+	let mut buf_l = VertexBuffer::from(left_side);
+	let mut buf_r = VertexBuffer::from(right_side);
+	let mut buf_t = VertexBuffer::from(top);
+
+	let pbuf_l = buf_l.rasterize_scan(&cfg);
+	let pbuf_r = buf_r.rasterize_scan(&cfg);
+	let pbuf_t = buf_t.rasterize_scan(&cfg);
 
 	// println!("{}", pbuf);
 
-	pbuf.render_pixels().await?;
+	pbuf_l.render_pixels().await?;
+	pbuf_r.render_pixels().await?;
+	pbuf_t.render_pixels().await?;
 
 	Ok(())
 }
